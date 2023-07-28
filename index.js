758c6cf1cf0206c5b94beba688d2b10c7be6b336
Kunal Rai <kunalrai@Kunals-MacBook-Air.local>
2023-07-28 22:59:45 +0530

    add: step 1 and step 2

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 // React.createElement
-const element = { 
-    type: 'h1',
-    props:{
-        title: 'foo',
-        children: 'Hello'
+
+// React.createElement('div', {id: 'foo'}, 'foo') => {type: 'div', props: {id: 'foo', children: "foo"}}
+function createTextElement(text){
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: []
+        }
+    };
+}
+
+function myCreateElement(type, props, ...children) {
+    return {
+        type,
+        props:{
+            ...props,
+            children: children.map(child => {
+                return typeof child === 'object' ? child : createTextElement(child)
+            })
+        }
     }
 }
+
+function myRender(element, container){
+    const dom =  element.type == "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(element.type)
+
+    const isProperty = key => key !== "children"
+    Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+
+    element.props.children.forEach(child => myRender(child, dom));
+
+    container.appendChild(dom);
+}
+
+const AutoDidact = {
+    myCreateElement,
+    myRender
+}
+
+// ----------------------------------------------------------------------
+
+/** @jsx AutoDidact.myCreateElement */
+// const element = (
+//     <div style="background: salmon">
+//       <h1>Hello World</h1>
+//       <h2 style="text-align:right">from AutoDidact</h2>
+//     </div>
+//   );
+
+  const element = AutoDidact.myCreateElement('h1', {id: 'foo'}, 'Hello');
 
 // Same
 const container = document.getElementById('root'); 
 
 // ReactDOM.render
-const node = document.createElement(element.type); 
-node['title'] = element.props.title;
-const text = document.createTextNode('');
-text['nodeValue'] = element.props.children;
-node.appendChild(text);
-container.appendChild(node);
+AutoDidact.myRender(element, container);
+
